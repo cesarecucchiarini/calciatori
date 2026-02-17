9f5fc28508987e0a11fa05a9089c28f517a48b63
cesarecucchiarini <cucchiarini.cesare@franchettisalviani.net>
2026-02-17 13:36:03 +0100

    a

---
+++ calciatori/src/calciatori/Calciatore.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calciatori;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author cucchiarini.cesare
+ */
+public class Calciatore {
+    private int goal;
+    private String nome;
+    private String nazionalita;
+    private LocalDate nascita;
+    private SegnoZodiacale segno;
+
+    public Calciatore(int goal, String nome, String nazionalita, LocalDate nascita) {
+        this.goal = goal;
+        this.nome = nome;
+        this.nazionalita = nazionalita;
+        this.nascita = nascita;
+    }
+    
+    public void calcolaSegno(){
+        
+    }
+}
---
+++ calciatori/src/calciatori/SegnoZodiacale.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package calciatori;
+
+import java.time.LocalDate;
+import java.time.MonthDay;
+import java.util.HashMap;
+
+/**
+ *
+ * @author cucchiarini.cesare
+ */
+public class SegnoZodiacale {
+    
+    private static HashMap<String, MonthDay[]> date = new HashMap<>();
+    
+    public static String getSegno(LocalDate data){
+        return "";
+    }
+    
+    public satic boolean isIn
+}
