483e5adc526e0b17360e403ab9fafb3da2b751bc
cesarecucchiarini <cucchiarini.cesare@franchettisalviani.net>
2026-02-23 09:02:52 +0100

    istruzioni

--- calciatori/src/calciatori/CalciatoriForm.java
+++ calciatori/src/calciatori/CalciatoriForm.java
@@ -9,6 +9,7 @@ import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
@@ -65,9 +66,21 @@ public class CalciatoriForm extends javax.swing.JFrame {
             }
         });
         
+        JButton bottoneIstruzioni = new JButton("Istruzioni");
+        bottoneIstruzioni.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                mostraIstruzioni();
+            }
+        });
+        
         bottone.setPreferredSize(new Dimension(300, 40));
+        bottoneIstruzioni.setPreferredSize(new Dimension(300, 40));
+        
         JPanel p = new JPanel();
+        p.setLayout(new GridLayout(1,2));
         p.add(bottone);
+        p.add(bottoneIstruzioni);
         this.add(p, BorderLayout.SOUTH);
     }
     
@@ -97,6 +110,19 @@ public class CalciatoriForm extends javax.swing.JFrame {
         scrollPane.repaint();
         resizeLabelFont(interfaccia);
     }
+    
+    private void mostraIstruzioni(){
+    String msg = """
+                 Questo progetto vuole dimostrare un rapporto tra lo zodiaco e la bravura a calcio.
+                 La prima interfaccia mostra le caratteristiche di ogni calciatore, preso da un file esterno.
+                 La seconda interfaccia mostra un istogramma che indica il rapporto dei goal tra i vari segni zodiacali.                    
+                 """;
+    
+    JLabel label = new JLabel("<html>" + msg.replace("\n", "<br>") + "</html>");
+    label.setFont(new Font("Arial", Font.PLAIN, 18));
+    
+    JOptionPane.showMessageDialog(this, label, "Istruzioni", JOptionPane.INFORMATION_MESSAGE);
+}
 
     /**
      * This method is called from within the constructor to initialize the form.
